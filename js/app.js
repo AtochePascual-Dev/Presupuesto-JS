dc4b54d05c8948eb7ea3da47313a0935f5daf597
Atoche Pascual <jmatochepascual@gmail.com>
2023-05-25 17:15:44 -1000

    [ADD] mostrarPresupuestoHtml

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 // * VARIABLES
 const formulario = document.querySelector('#agregar-gasto');
 const gastoListado = document.querySelector('#gastos ul');
-const objPresupuestp = {};
+let presupuesto;
 
+
+// * CLASES
+class Presupuesto {
+  constructor(presupuesto) {
+    this.presupuesto = presupuesto;
+    this.restante = presupuesto;
+    this.gastos = [];
+  }
+};
+
+
+class UI {
+
+  // * Muestra presupuesto en el html
+  mostrarPresupuestoHtml(presupuestoOBJ) {
+    const { presupuesto, restante } = presupuestoOBJ;
+
+    const presupuestoHtml = document.querySelector('#total');
+    const restanteHtml = document.querySelector('#restante');
+
+    presupuestoHtml.textContent = presupuesto;
+    restanteHtml.textContent = restante;
+  };
+};
+
+const ui = new UI();
 
 // * EVENTOS
 // * Cuando el documento esta listo
@@ -10,7 +36,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // Despues de 1segundo sollicitamos el presupuesto
   setTimeout(() => {
     solicitarPresupuesto();
-  }, 500);
+    ui.mostrarPresupuestoHtml(presupuesto);
+  }, 1200);
 });
 
 
@@ -22,6 +49,8 @@ const solicitarPresupuesto = () => {
 
   do {
     presupuestoUsuario = Number(prompt('Ingrese un presupuesto'));
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
 
     // Miestras el presupuesto no sea el indicado seguir solicitnadolo
   } while (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0);
